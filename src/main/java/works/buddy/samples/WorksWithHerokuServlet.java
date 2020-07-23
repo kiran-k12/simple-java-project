fe07a3f9f62ed76ae359c638ea4b9d695947b1cc
kiran-k12 <62979107+kiran-k12@users.noreply.github.com>
2020-07-23 18:58:38 +0530

    Update WorksWithHerokuServlet.java

--- src/main/java/works/buddy/samples/WorksWithHerokuServlet.java
+++ src/main/java/works/buddy/samples/WorksWithHerokuServlet.java
@@ -16,4 +16,8 @@ public class WorksWithHerokuServlet extends HttpServlet {
         writer.print("Buddy Works with Heroku");
         writer.close();
     }
+    
+    public void methodTest(int input1, int input2, int input3, int input4, int input5, int input6){ 
+    System.out.println(input1+input2+input3+input4+input5+input6);
+ } 
 }
